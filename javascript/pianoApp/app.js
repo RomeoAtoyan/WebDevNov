5692bf2cc443df0eb9c0ad18356e6fd3f570c6c0
RomeoAtoyan <atoyanr@gmail.com>
2021-12-20 17:18:42 +0100

    1718

---
+++ javascript/pianoApp/app.js
@@ -0,0 +1,35 @@
+// let allbuttons = document.querySelectorAll('.key')
+
+// allbuttons.forEach(el=>{
+//     console.log(el)
+//     console.log(el.getAttribute('data-key'))
+//     // set action
+//     el.addEventListener('click',function(e){
+//         console.log('click')
+//         // play audio
+//     })
+// })
+
+
+let notes = document.querySelectorAll('audio')
+let buttons = document.querySelectorAll('.key')
+
+function PlayAudio(keyNumber){
+    notes.forEach(audio=>{
+        let getKeyOfAudio = audio.getAttribute('data-key')
+       // console.log(getKeyOfAudio)
+     //   audio.play()
+        if(getKeyOfAudio == keyNumber){
+            audio.play();
+        }
+    })
+}
+
+buttons.forEach(el=>{
+    el.addEventListener('click',function(e){
+        e.preventDefault()
+        let getKey = el.getAttribute('data-key')
+        PlayAudio(getKey)
+    })
+    el.addEventListener('')
+})
