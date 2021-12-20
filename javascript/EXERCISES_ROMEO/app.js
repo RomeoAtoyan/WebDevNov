9ef581b41a03ddd217029abc4c3df6afce1d2af3
RomeoAtoyan <atoyanr@gmail.com>
2021-12-20 17:35:28 +0100

    1735

---
+++ javascript/EXERCISES_ROMEO/app.js
@@ -0,0 +1,24 @@
+let enger = 'romeo'
+
+enger? console.log(`Hello ${enger}!`) : console.log('Hello')
+
+switch(enger){
+    case 'Arsen':
+    case 'arsen':
+    console.log('You have a CM16')
+    break
+    case 'Xcho':
+    case 'xcho':
+    console.log('You have an AK74')
+    break
+    case 'Gevorg':
+    case 'gevorg':
+    console.log('You have an M4')
+    break
+    case 'Romeo':
+    case 'romeo':
+    console.log('You have a Bolt M4')
+    break
+    default:
+    console.log("You dont play airsoft")               
+}
