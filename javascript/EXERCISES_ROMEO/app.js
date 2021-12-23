f63d81944ffdf2efe674be4baedff1768ee23fab
RomeoAtoyan <atoyanr@gmail.com>
2021-12-23 09:05:49 +0100

    0905

--- javascript/EXERCISES_ROMEO/app.js
+++ javascript/EXERCISES_ROMEO/app.js
@@ -1,24 +1,42 @@
-let enger = 'gevorg'
+// let enger = 'xcho'
 
-enger? console.log(`Hello ${enger}!`) : console.log('Hello')
+// enger? console.log(`Hello ${enger}!`) : console.log('Hello')
 
-switch(enger){
-    case 'Arsen':
-    case 'arsen':
-    console.log('You have a CM16')
-    break
-    case 'Xcho':
-    case 'xcho':
-    console.log('You have an AK74')
-    break
-    case 'Gevorg':
-    case 'gevorg':
-    console.log('You have an M4')
-    break
-    case 'Romeo':
-    case 'romeo':
-    console.log('You have a Bolt M4')
-    break
-    default:
-    console.log("You dont play airsoft")               
+// switch(enger){
+//     case 'Arsen':
+//     case 'arsen':
+//     console.log('You have a CM16')
+//     break
+//     case 'Xcho':
+//     case 'xcho':
+//     console.log('You have an AK74')
+//     break
+//     case 'Gevorg':
+//     case 'gevorg':
+//     console.log('You have an M4')
+//     break
+//     case 'Romeo':
+//     case 'romeo':
+//     console.log('You have a Bolt M4')
+//     break
+//     default:
+//     console.log("You dont play airsoft")               
+// }
+
+// const basis = 50
+// const hoogte = 25
+
+// function rechtHoek (basis,hoogte) {
+//     console.log( basis * hoogte)
+// }
+
+// rechtHoek(50,25)
+
+const basis = 10
+const hoogte = 5
+
+function parallellogram(basis,hoogte) {
+    console.log((basis * hoogte) / 2)
 }
+parallellogram(10,5)
+
